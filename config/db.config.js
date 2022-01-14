9757f1584928ca2f11ac3a38f583a5f5f4804e27
MaquiaveloAlpha <rraygoza1408@gmail.com>
2022-01-13 21:57:12 -0600

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "us-cdbr-east-05.cleardb.net",
-    USER: "b08b9dd17f049a",
-    PASSWORD: "d7656b84",
-    DB: "heroku_b15fe88b3090ab8",
+    HOST: "",
+    USER: "",
+    PASSWORD: "",
+    DB: "",
     dialect: "mysql",
     pool: {
         max: 5,
